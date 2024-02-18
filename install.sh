ed5c363e5cae4f17f31daa6c6b69b7a191279f54
ManiacBoy777 <rkm0722@gmail.com>
2024-02-17 23:26:09 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,3 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i '/plugins=(/a \  git \' ~/.zshrc
-sed -i '/plugins=(/a \  fzf-tab \' ~/.zshrc
-sed -i '/plugins=(/a \  zsh-autosuggestions \' ~/.zshrc
-sed -i '/plugins=(/a \  zsh-syntax-highlighting \' ~/.zshrc
